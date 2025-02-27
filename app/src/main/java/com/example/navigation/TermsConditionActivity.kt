dc7b7747e796e2114535a938c3e9b93995dc42a0
ikeerwp <ikeerwp05@gmail.com>
2025-02-27 21:32:53 +0100

    push dia Iker

---
+++ app/src/main/java/com/example/navigation/TermsConditionActivity.kt
@@ -0,0 +1,11 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.navigation.R
+
+class TermsConditionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_conditions)
+    }
+}
