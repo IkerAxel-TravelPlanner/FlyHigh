8be55fdc53d55d1ee48c2a1d39f4b8d043546fc4
axeelbl <axelbl54@gmail.com>
2025-03-01 20:45:31 +0100

    T3 Hecho - Axel (En principio está todo falta comparar conparar sergio)

---
+++ app/src/main/java/com/example/navigation/modules/ItineraryItem.java
@@ -0,0 +1,21 @@
+package com.example.navigation.modules;
+
+import android.location.Location;
+
+import java.util.Date;
+
+public class ItineraryItem {
+    private String itemId;
+    private String title;
+    private String description;
+    private Date startTime;
+    private Date endTime;
+    private Location location;
+
+    public void editItem() {
+        //TODO
+    }
+    public void deleteItem() {
+        //TODO
+    }
+}
---
+++ app/src/main/java/com/example/navigation/modules/Map.java
@@ -0,0 +1,9 @@
+package com.example.navigation.modules;
+
+import java.util.List;
+
+public class Map {
+    public void showLocation(double latitude, double longitude) {}
+    public List<String> getNearbyPlaces(String category) { return null; }
+    //public Route getDirections(Location start, Location end) { return null; }
+}
