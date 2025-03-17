ef715f60ea03dba6775ffef7b7db84b39c8fb1e6
axeelbl <axelbl54@gmail.com>
2025-03-17 18:57:56 +0100

    Version 1.1.0

--- app/src/main/java/com/example/navigation/modules/Trip.kt
+++ app/src/main/java/com/example/navigation/modules/Trip.kt
@@ -3,17 +3,12 @@ package com.example.navigation.modules
 import android.media.Image
 import java.util.Date
 
-class Trip(
+data class Trip(
     var tripId: String,
     var name: String,
     var startDate: Date,
     var endDate: Date,
     var destination: String,
-    var itinerary: List<ItineraryItem>,
-    var photos: List<Image>
-) {
-    fun addItineraryItem(item: ItineraryItem) {}
-    fun removeItineraryItem(item: ItineraryItem) {}
-    fun uploadPhoto(img: Image) {}
-    fun shareTrip(user: User) {}
-}
+    var itinerary: List<ItineraryItem> = listOf(),
+    var photos: List<Image> = listOf()
+)
