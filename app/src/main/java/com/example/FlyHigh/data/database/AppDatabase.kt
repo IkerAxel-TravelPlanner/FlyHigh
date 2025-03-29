b06b4b8dafa2c72faf77c9dd1d433bebf68d53be
axeelbl <axelbl54@gmail.com>
2025-03-29 20:46:10 +0100

    version 0.3.0 T1 Completed

--- app/src/main/java/com/example/FlyHigh/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/FlyHigh/data/database/AppDatabase.kt
@@ -1,18 +1,34 @@
 package com.example.FlyHigh.data.database
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.example.FlyHigh.data.Converters
 import com.example.FlyHigh.data.local.dao.ItineraryItemDao
 import com.example.FlyHigh.data.local.dao.TripDao
-import com.example.FlyHigh.domain.model.ItineraryItem
-import com.example.FlyHigh.domain.model.Trip
+import android.content.Context
+import androidx.room.*
+import com.example.FlyHigh.data.Converters
+import com.example.FlyHigh.data.local.dao.UserDao
+import com.example.FlyHigh.domain.model.*
 
-@Database(entities = [Trip::class, ItineraryItem::class], version = 1)
-@TypeConverters(Converters::class) // This is the correct one //  //
+@Database(entities = [Trip::class, ItineraryItem::class, UserEntity::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun tripDao(): TripDao
     abstract fun itineraryItemDao(): ItineraryItemDao
-}
+    abstract fun userDao(): UserDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "flyhigh_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
